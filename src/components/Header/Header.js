3b248e3876527ea606c504fcc7cb0b5bcd0fca34
Arceniy Khramtsov <Dubsta.post@yandex.ru>
2020-01-03 02:35:02 +0300

    Layout and components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.png';
+const Header = () => {
+    return (
+        <header className="header">
+            <img src={logo}></img>
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+
+const Profile = () => {
+    return (
+        <main className="content">
+            <div>
+                <img src="https://s1.1zoom.me/big3/147/Waterfalls_Summer_Rivers_Rays_of_light_563524_2800x1874.jpg" />
+            </div>
+            <div>
+                ava + desc
+			</div>
+            <div>               
+				<div>
+                    div1
+				</div>
+                <div>
+                    div2
+				</div>
+            </div>
+        </main>
+    );
+}
+
+export default Profile;
